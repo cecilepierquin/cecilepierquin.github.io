06d287fecd76a6b2b3cc9dbc1254ef6897b3e03b
Raphael Pierquin <raphael@ut7.fr>
2015-07-22 10:22:25 +0200

    une barre de navigation responsive

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+/* sur le mobile, force la navigation à se replier après un clic */
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});

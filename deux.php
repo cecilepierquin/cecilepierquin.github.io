445aaf198b0fb8b42749f20a260b1a3d9744d859
Raphael Pierquin <raphael@ut7.fr>
2015-07-27 08:04:50 +0200

    deuxième page

---
+++ deux.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include 'includes/head.html'?>
+<?php include 'includes/navbar.html'?>
+
+    <section id="design-selection">
+      <div class="container">
+        <div class="row">
+          <h2>Interactive graphic design</h2>
+            <div class="col-lg-12 text-center">
+            Selection of 
+            <span class="gros">personal</span> and 
+            <span class="moyen">commissioned projects</span>
+            <br>
+            <span class="highlight"> advertising agencies </span> ,
+            <span class="highlight"> publishers </span> and
+            <span class="highlight"> private companies </span>.
+          </div>
+        </div>
+      </div>
+      <div class="container selection">
+        <div class="row">
+          <div class="col-sm-12">
+            <img src="images/design/cecile-pierquin_exemple1.jpg" class="img-responsive">
+            <div class="legende">
+              <p>Online advertising for <strong>EDF-ENR</strong>, the french national provider of solar energy
+              <p class="petit">Newsletters, web banners, mail, landing pages, Facebook covers, visual, etc ...
+              <p class="trespetit">Commionned by <strong>EDF-ENR</strong> (Paris France)
+            </div>
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6">
+            <img src="images/design/cecile-pierquin_exemple2.jpg" class="img-responsive">
+            <div class="legende">
+              <p>Online advertising for <strong>EDF-ENR</strong>
+              <p class="petit">Newsletters, web banners, mail, landing pages
+              <p class="trespetit">Commionned by <strong>EDF-ENR</strong> (Paris France)
+            </div>
+            <hr>
+          </div>
+          <div class="col-sm-6">
+            <img src="images/design/cecile-pierquin_exemple2.jpg" class="img-responsive">
+            <div class="legende">
+              <p>Online advertising for <strong>EDF-ENR</strong>
+              <p class="petit">Newsletters, web banners, mail, landing pages
+              <p class="trespetit">Commionned by <strong>EDF-ENR</strong> (Paris France)
+            </div>
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <img src="images/design/cecile-pierquin_exemple3.jpg" class="img-responsive">
+            <div class="legende">
+              <p class="petit">Websites, Intranet, animation for <strong>Danone</strong>
+              <p class="trespetit">Commionned by <strong>Angie</strong> (Paris France)
+            </div>
+            <hr>
+          </div>
+          <div class="col-sm-4">
+            <img src="images/design/cecile-pierquin_exemple3.jpg" class="img-responsive">
+            <div class="legende">
+              <p class="petit">Websites, Intranet, animation for <strong>Danone</strong>
+              <p class="trespetit">Commionned by <strong>Angie</strong> (Paris France)
+            </div>
+            <hr>
+          </div>
+          <div class="col-sm-4">
+            <img src="images/design/cecile-pierquin_exemple3.jpg" class="img-responsive">
+            <div class="legende">
+              <p class="petit">Websites, Intranet, animation for <strong>Danone</strong>
+              <p class="trespetit">Commionned by <strong>Angie</strong> (Paris France)
+            </div>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <img src="images/cecile-pierquin_line-beach.jpg" class="img-responsive"/>
+        </div>
+      </div>
+    </section>
+
+<?php include 'includes/scripts.html'?>
+<?php include 'includes/footer.html'?>
+  </body>
+</html>
+

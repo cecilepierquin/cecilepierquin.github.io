fbceb401aae82c7a18e748e8daab61fcb18185ab
Cécile Pierquin <cecile@pierquin.com>
2015-08-10 13:01:35 +0200

    Ajouté des projets dans "Visual Design"

--- js/custom.js
+++ js/custom.js
@@ -25,25 +25,25 @@ $(document).ready(function(){
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 4
+          slidesToShow: 5
         }
       },
       {
         breakpoint: 970,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 4
         }
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 3
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 2
         }
       }
     ]
